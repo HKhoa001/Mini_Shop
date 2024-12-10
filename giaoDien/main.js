dbb199e75d1a07e3caa2c5c6103d55b895c02951
HuyHa <107118105+HaHuy34@users.noreply.github.com>
2024-12-10 17:30:09 +0700

    update code

--- giaoDien/main.js
+++ giaoDien/main.js
@@ -1,11 +1,11 @@
 const swiper = new Swiper('.swiper', {
-    loop: true, // Tạo hiệu ứng lặp vô hạn
-    autoplay: {
-      delay: 1000, // Thời gian giữa mỗi lần chuyển slide (1 giây)
-      disableOnInteraction: false, // Không dừng khi người dùng tương tác
-    },
-    pagination: {
-      el: '.swiper-pagination',
-      clickable: true, // Cho phép nhấp vào pagination để chọn slide
-    },
-  });
+  loop: true, // Tạo hiệu ứng lặp vô hạn
+  autoplay: {
+    delay: 1000, // Thời gian giữa mỗi lần chuyển slide (1 giây)
+    disableOnInteraction: false, // Không dừng khi người dùng tương tác
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true, // Cho phép nhấp vào pagination để chọn slide
+  },
+});
