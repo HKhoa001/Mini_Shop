7993988fcf6507f40ebfaa9a7a1f00acedb8e6c1
HuyHa <107118105+HaHuy34@users.noreply.github.com>
2024-12-10 19:03:53 +0700

    update code template

--- main.js
+++ main.js
@@ -1,11 +1,1 @@
-// const swiper = new Swiper('.swiper', {
-//   loop: true, // Tạo hiệu ứng lặp vô hạn
-//   autoplay: {
-//     delay: 1000, // Thời gian giữa mỗi lần chuyển slide (1 giây)
-//     disableOnInteraction: false, // Không dừng khi người dùng tương tác
-//   },
-//   pagination: {
-//     el: '.swiper-pagination',
-//     clickable: true, // Cho phép nhấp vào pagination để chọn slide
-//   },
-// });
+  AOS.init();
